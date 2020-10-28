de6ab0ead80ba6db20e5789a82c247299925a9c8
ibrohimi9933 <73479648+ibrohimi9933@users.noreply.github.com>
2020-10-28 08:48:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Ibrohim Islamov"
+let userName = "ibrohimi9933";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
